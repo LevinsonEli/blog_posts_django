9930510622260a39e6f5b84b26120a246e555434
unknown <eliyahulevinson@gmail.com>
2023-06-08 17:18:00 -0400

    added edit post route

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('editPost/<int:id>', views.editPost, name='editPost'),
     path('<int:id>', views.post, name='post'),
     path('', views.index, name='index'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -9,3 +9,15 @@ def index(req):
 def post(req, id):
     post = Post.objects.get(id = id)
     return render(req, 'post.html', { 'post': post })
+
+def editPost(req, id):
+    if req.method == "GET":
+        post = Post.objects.get(id = id)
+        return render(req, 'editPost.html', { 'post': post })
+    elif req.method == "POST":
+        post = Post.objects.get(id = id)
+        post.title = req.POST["title"]
+        post.body = req.POST["body"]
+        post.save()
+        return render(req, 'post.html', { 'post': post })
+
